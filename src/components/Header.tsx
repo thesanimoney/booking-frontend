838a0b1424c875de4f9b0b9565afe9b2c5f02e74
thesanimoney <thesanimoney@gmail.com>
2024-06-09 23:16:29 +0300

    added images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import TypographyH2 from "@/components/typography/TypographyH2.tsx";
 import List from "@/components/List.tsx";
-import {navLinks} from "@/data/data.ts";
+import {blackLogo, navLinks, whiteLogo} from "@/data/data.ts";
 import {useTheme} from "@/components/theme-provider.tsx";
 import MenuGroup from "@/components/MenuGroup.tsx";
 
@@ -10,7 +10,7 @@ function Header() {
     return <>
         <div className="mx-auto flex justify-between items-center">
             <div className="flex items-center gap-2">
-                <img src={theme == 'dark' ? "src/assets/logoWhite.png" : "src/assets/logoBlack.png"} alt="logo"
+                <img src={theme == 'dark' ? `${whiteLogo}` : `${blackLogo}`} alt="logo"
                      className={'h-12 w-12 -translate-y-1'}/>
                 <TypographyH2 text={'Sani Estate'}/></div>
             <div className="hidden flex-row gap-10 flex-wrap lg:flex">
--- src/layout/imageBlock.tsx
+++ src/layout/imageBlock.tsx
@@ -1,13 +1,15 @@
+import {skyscraper, skyscraperDark} from "@/data/data.ts";
+
 const ImageSection = () => {
     return (
         <section className="grid grid-cols-2 gap-4 p-4">
             <img
-                src="src/assets/skyscrapper1.jpg"
+                src={skyscraper}
                 alt="Image 1"
                 className="w-full h-auto rounded-lg shadow-2xl drop-shadow-2xl transform transition-transform duration-300 ease-in-out hover:scale-125 -translate-y-10 translate-x-20"
             />
             <img
-                src="src/assets/skyscrapperNight.jpg"
+                src={skyscraperDark}
                 alt="Image 2"
                 className="w-full h-auto rounded-lg shadow-2xl drop-shadow-2xl transform transition-transform duration-300 ease-in-out translate-y-10 hover:scale-125"
             />
