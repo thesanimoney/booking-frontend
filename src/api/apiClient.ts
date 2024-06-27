ff687cceb3b09823aa7418a200b5fee8a1f219e6
thesanimoney <thesanimoney@gmail.com>
2024-06-28 01:37:20 +0300

    slider + filtration

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_API,
+    baseURL: 'http://localhost:3000/api',
 })
 
 export default apiClient;
